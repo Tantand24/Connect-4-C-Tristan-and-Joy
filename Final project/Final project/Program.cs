1bae79d25504007e514ab42e79e27598c1ad2b17
Tantand24 <tristan.dang@email.com>
2025-06-06 18:55:28 -0600

    Starting stuff

---
+++ Final project/Final project/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Final_project
+{
+    public abstract class player
+    {
+        public string Name { get; set; }
+        public bool XorO { get; set; }
+
+        public virtual void MakeMove()
+        {
+
+        }
+    }
+
+    public class human : player
+    {
+
+    }
+
+    //public class AI : player
+
+    public class board
+    {
+        public board[[6][6][6][6][6][6][6]];
+        public bool IsGameEnd = false;
+
+        public void DisplayBoard()
+        {
+
+        }
+
+        public void CheckWin()
+        {
+
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
