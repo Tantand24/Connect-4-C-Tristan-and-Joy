30b72b9f3a8f116fcdbc73d3f9fb120bc690ad15
gjoyfrancisco <fr.grecille@gmail.com>
2025-06-11 12:47:58 -0600

    Update Program.cs

--- Final project/Final project/Program.cs
+++ Final project/Final project/Program.cs
@@ -1,9 +1,15 @@
 ﻿namespace Final_project
 {
-    public abstract class player
+    public abstract class Player
     {
         public string Name { get; set; }
         public bool XorO { get; set; }
+
+        public Player(string name, string xoro)
+        {
+              Name = name;
+              XorO = xoro;
+        }
 
         public virtual void MakeMove()
         {
@@ -11,14 +17,21 @@
         }
     }
 
-    public class human : player
+    public class Human :Player
     {
+        public Human(string name, string xoro) : base(name, xoro){}
 
+        public override MakeMove(Board board)
+        {
+             Console.WriteLine($"{Name}, Make your move (1-7): ");
+             int column; 
+        }
+        
     }
 
     //public class AI : player
 
-    public class board
+    public class Board
     {
         public board[[6][6][6][6][6][6][6]];
         public bool IsGameEnd = false;
